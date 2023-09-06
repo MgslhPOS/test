66fffc8b8f4a7682a21bf4aee732b3a521824d5a
Azzam <azzammgslh@gmail.com>
2023-09-06 17:06:29 +0300

    first commit

---
+++ config/version.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'current' => '1.24.0'
+
+];
---
+++ resources/views/components/ui/share.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes }}>
+    <div class="flex justify-around flex-wrap">
+      
+        
+        <a href="/path/to/generate/pkpass" class="btn btn-primary">إضافة لمحفظة Apple</a>
+    </div>
+</div>
